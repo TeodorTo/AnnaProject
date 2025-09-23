69f4794cd45ffbc4f37b2e253f5945606ce71e66
MarkVoid <mark8ivanov@gmail.com>
2025-09-23 15:17:20 +0300

    migrations for category image

---
+++ Infrastructure/Migrations/20250923115252_Add image to Category.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddimagetoCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Slug",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "Categories",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Slug",
+                table: "Categories",
+                column: "Slug",
+                unique: true,
+                filter: "[Slug] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Slug",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "Categories",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Slug",
+                table: "Categories",
+                column: "Slug",
+                unique: true);
+        }
+    }
+}
